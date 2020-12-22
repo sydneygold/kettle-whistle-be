68370c33733c9689e331c35940353d4d66c5ce59
sydneygold <sydney.goldberg@outlook.com>
2020-12-21 17:53:42 -0700

    added controllers for index and show

--- app/controllers/coffees_controller.rb
+++ app/controllers/coffees_controller.rb
@@ -1,2 +1,15 @@
 class CoffeesController < ApplicationController
+
+    def index 
+        @coffees = Coffee.all
+
+        render json: @coffees
+    end
+
+    def show
+        @coffee = Coffee.find(params[:id])
+
+        render json: @coffee
+    end
+
 end
--- app/controllers/teas_controller.rb
+++ app/controllers/teas_controller.rb
@@ -1,2 +1,14 @@
 class TeasController < ApplicationController
+
+    def index 
+        @teas = Tea.all
+
+        render json: @teas
+    end
+
+    def show
+        @tea = Tea.find(params[:id])
+
+        render json: @tea
+    end
 end
